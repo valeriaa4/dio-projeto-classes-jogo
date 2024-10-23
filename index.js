91f179dab291ad55c4e24ee88209ca8bc745a864
valeriaa4 <santosvaleriav4@gmail.com>
2024-10-23 11:48:45 -0300

    método ataque

--- index.js
+++ index.js
@@ -6,4 +6,19 @@ class heroe{
 		this.age = age
 		this.type = type // Guerreiro, Mago, Monge, Ninja
 	}
+    
+    attack(){
+		if(this.type == "Guerreiro"){
+			console.log(`O ${this.type} atacou usando espada!`)
+		}
+		if(this.type == "Mago"){
+			console.log(`O ${this.type} atacou usando magia!`)
+		}
+		if(this.type == "Monge"){
+			console.log(`O ${this.type} atacou usando artes marciais!`)
+		}
+		if(this.type == "Ninja"){
+			console.log(`O ${this.type} atacou usando shuriken!`)
+		}
+	}
 }
