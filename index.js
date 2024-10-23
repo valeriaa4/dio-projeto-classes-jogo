9e12c634ca4bf85ee3293cf3a1977c00151d659d
valeriaa4 <santosvaleriav4@gmail.com>
2024-10-23 11:57:21 -0300

    instanciando objetos e saída

--- index.js
+++ index.js
@@ -6,19 +6,29 @@ class heroe{
 		this.age = age
 		this.type = type // Guerreiro, Mago, Monge, Ninja
 	}
-    
+
     attack(){
 		if(this.type == "Guerreiro"){
-			console.log(`O ${this.type} atacou usando espada!`)
+			console.log(`O ${this.type} ${this.name} atacou usando espada!`)
 		}
 		if(this.type == "Mago"){
-			console.log(`O ${this.type} atacou usando magia!`)
+			console.log(`O ${this.type} ${this.name} atacou usando magia!`)
 		}
 		if(this.type == "Monge"){
-			console.log(`O ${this.type} atacou usando artes marciais!`)
+			console.log(`O ${this.type} ${this.name} atacou usando artes marciais!`)
 		}
 		if(this.type == "Ninja"){
-			console.log(`O ${this.type} atacou usando shuriken!`)
+			console.log(`O ${this.type} ${this.name} atacou usando shuriken!`)
 		}
 	}
 }
+
+let heroe1 = new heroe("Json", 49, "Guerreiro")
+let heroe2 = new heroe("Rasmodius", 102, "Mago")
+let heroe3 = new heroe("Herobrine", 21, "Monge")
+let heroe4 = new heroe("Jackie Chan", 70, "Ninja")
+
+heroe1.attack()
+heroe2.attack()
+heroe3.attack()
+heroe4.attack()
